ee7b7cb662a78886ac13658cb4e6c6d1769f39c4
ammatsui <54817160+ammatsui@users.noreply.github.com>
2021-01-13 00:40:59 +0200

    added fast modular exponentiation

--- rsa.hs
+++ rsa.hs
@@ -1,11 +1,17 @@
+import Data.Bits
+
+modExp :: Integer -> Integer -> Integer -> Integer
+modExp b 0 m = 1
+modExp b e m = t * modExp ((b * b) `mod` m) (shiftR e 1) m `mod` m where t = if testBit e 0 then b `mod` m else 1
+
 
 -- encryption
 encr :: Integer -> Integer -> Integer -> Integer
-encr phi e n = mod ( phi ^ e ) n
+encr phi e n = modExp phi e n
 
 -- decryption
 decr :: Integer -> Integer -> Integer -> Integer
-decr c d n = mod ( c ^ d ) n
+decr c d n = modExp c d n
 
 
 -- take heed, o eager being!
